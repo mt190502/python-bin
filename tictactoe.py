ed3fdfe4ad15272cca08d5abceb1e7c28bd2c1af
Taha <62564400+discover02@users.noreply.github.com>
2021-12-28 16:56:35 +0300

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,165 @@
+import random, os, time
+
+class tictactoe:
+    firstgame = 1
+    lastplayer = ""
+    brd = []
+    termcls = os.get_terminal_size().columns
+    players = {}
+
+    def __init__(self, pl1=None, pl2=None, width=None):
+        if ((pl1 == None) or (pl1 == "")):
+            print("WARN: Player 1 didn't entered a name, \"Player1\" name setted by default")
+            self.pl1 = "Player1"
+            self.players["Player1"] = "X"
+        else:
+            self.pl1 = pl1
+            self.players[self.pl1] = "X"
+
+        if ((pl2 == None) or (pl2 == "")):
+            print("WARN: Player 2 didn't entered a name, \"Player2\" name setted by default")
+            self.pl2 = "Player2"
+            self.players["Player2"] = "Y"
+        else:
+            self.pl2 = pl2
+            self.players[self.pl2] = "Y"
+
+        if ((width == None) or (width == "")):
+            print("WARN: Board size not entered, \"5\" is setted by default.")
+            self.width = 5
+        else:
+            self.width = width
+
+        if int(self.width) <= 2:
+            print("You can make at least 3x3 boards")
+            exit()
+        elif int(self.width) >= 10:
+            print("You can make at most 10x10 boards")
+            exit()
+
+        time.sleep(3)
+
+    def genboard(self):
+        line = ["___"] * self.width
+
+        for x in range(self.width):
+            self.brd.append(line.copy())
+
+
+    def printboard(self):
+        if os.name != "nt":
+            os.system("clear")
+        else:
+            os.system("cls")
+
+        abc = [f"{i}  " for i in range(1, self.width+1)]
+        print("  ".join(abc).center(int(self.termcls)), "")
+
+        tmpbrd = []
+        for a in self.brd:
+            tmpbrd.append(a.copy())
+
+        for b in range(1, len(tmpbrd)+1):
+            tmpbrd[b-1].insert(0, f"{b} ")
+
+        for c in tmpbrd:
+            print("  ".join(c).center(int(self.termcls-5)), "\n")
+    
+
+
+
+    def playerselection(self):
+        if self.firstgame == 1:
+            self.lastplayer = random.randint(0, 1)
+
+        if self.lastplayer == 0:
+            self.lastplayer = 1
+            return self.pl1
+        elif self.lastplayer == 1:
+            self.lastplayer = 0
+            return self.pl2
+
+
+
+    def applymove(self, curpl):
+        self.a = curpl
+
+        if self.brd[self.slocation[0]][self.slocation[1]] == "___":
+            self.brd[int(self.slocation[0])][int(self.slocation[1])] = self.a.center(3)
+            return True
+        else:
+            print("This location is full, please choose another location!..")
+            time.sleep(2)
+            return False
+
+
+    def checkplayer(self, player):
+        for a in self.brd:
+            if ("".join(a).replace(" ", "").find(player*3)) != -1:
+                return True
+
+
+        for b in range(0, len(self.brd)):
+            chars = ""
+            for c in range(0, len(self.brd)):
+                chars += self.brd[c][b]
+            
+            if (chars.replace(" ", "").find(player*3)) != -1:
+                return True
+
+        for d in range(1, len(self.brd) - 1):
+            for e in range(1, len(self.brd) - 1):
+                chars2 = [self.brd[d-1][e-1], self.brd[d][e], self.brd[d+1][e+1]]
+                chars3 = [self.brd[d+1][e-1], self.brd[d][e], self.brd[d-1][e+1]]
+                
+                if ("".join(chars2).replace(" ", "").find(player*3)) != -1:
+                    return True
+                
+                if ("".join(chars3).replace(" ", "").find(player*3)) != -1:
+                    return True
+
+
+
+    def action(self):
+        self.curpl = self.playerselection()
+        
+        self.firstgame = 0
+
+        while True:
+            self.printboard()
+
+            print(f"Order of {self.curpl}")
+
+            try:
+                self.locationx = int(input("Enter the vertical position: "))
+                self.locationy = int(input("Enter the horizontal position: "))
+            except ValueError:
+                print("Please type a number.")
+                time.sleep(2)
+            else:
+                try:
+                    self.slocation = [int(self.locationx) - 1, int(self.locationy) - 1]
+            
+                    if self.applymove(self.players.get(self.curpl)) == True:
+                        if self.checkplayer(self.players.get(self.curpl)) == True:
+                            self.printboard()
+                            print(f"{self.curpl} wins!!!")
+                            exit()
+                        self.curpl = self.playerselection()
+                except IndexError:
+                    print(f"Choose a number between 1 and {self.width}.")
+                    time.sleep(2)
+
+
+
+    def run(self):
+        self.genboard()
+        self.action()
+
+
+pl1 = input("Player1 name:\n> ")
+pl2 = input("Player2 name:\n> ")
+width = input("Board size (min 3, max 9)\n> ")
+
+gamesession1 = tictactoe(pl1, pl2, width)
+gamesession1.run()
